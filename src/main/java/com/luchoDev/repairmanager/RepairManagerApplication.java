28dcaeb820f115f0dabfa666bc5a052781cc4ac5
Luis <lgimenez@frba.utn.edu.ar>
2022-05-18 20:44:10 -0300

    initial commit

---
+++ src/main/java/com/luchoDev/repairmanager/RepairManagerApplication.java
@@ -0,0 +1,13 @@
+package com.luchoDev.repairmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luchoDev/repairmanager/RepairManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luchoDev.repairmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
