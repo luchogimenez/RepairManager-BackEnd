db21d22af641fa7c525923df0aa6f6519cd2c610
lgimenez <lgimenez@sysone.com>
2022-07-03 20:42:35 -0300

    se corrige entidad User agregando fetch EAGER en relacion manyToMany con Role

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # RepairManager-BackEnd
+
+sudo docker run --name postgresql-container -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=password -e POSTGRES_DB=testdb -d postgres  
+sudo docker ps -a  
+sudo docker start CONTAINER_ID
--- src/main/java/com/luchoDev/repairmanager/security/entity/UserDTO.java
+++ src/main/java/com/luchoDev/repairmanager/security/entity/UserDTO.java
@@ -27,7 +27,7 @@ public class UserDTO {
     @NotNull
     private String password;
     @NotNull
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_role",joinColumns = @JoinColumn(name="user_id"),
     inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
